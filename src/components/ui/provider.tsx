b14c9854266dbbd3d9209ebe60f175634bdc6708
jsfather <keyvanmatinrad@outlook.com>
2026-05-05 09:46:34 +0330

    vazirmatn font added and used

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,14 +1,25 @@
 "use client"
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        body: { value: "'Vazirmatn', sans-serif" },
+        heading: { value: "'Vazirmatn', sans-serif" },
+      },
+    },
+  },
+})
+
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   )
